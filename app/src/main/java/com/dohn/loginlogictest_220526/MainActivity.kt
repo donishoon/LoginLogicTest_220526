29623dafcd203fb3a561ca47096ca250cb5c644d
강도훈 <105531571+donishoon@users.noreply.github.com>
2022-05-26 23:35:16 +0900

    Initial commit

---
+++ app/src/main/java/com/dohn/loginlogictest_220526/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.dohn.loginlogictest_220526
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnCheck.setOnClickListener {
+
+            if (  )
+
+        }
+
+    }
+}
