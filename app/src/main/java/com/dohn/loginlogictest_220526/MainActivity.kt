d510096001460f1ef55d45e22d13565e3598c26c
강도훈 <105531571+donishoon@users.noreply.github.com>
2022-05-26 23:56:10 +0900

    연습문제_로그인_토스트 표현

--- app/src/main/java/com/dohn/loginlogictest_220526/MainActivity.kt
+++ app/src/main/java/com/dohn/loginlogictest_220526/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dohn.loginlogictest_220526
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,9 +10,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         btnCheck.setOnClickListener {
 
-            if (  )
+//            버튼이 눌리면  > 입력한 id /pw를 변수로 저장
+
+            val inputId = edtId.text.toString()
+            val inputPw = edtPassword.text.toString()
+
+            if ( inputId == "admin" && inputPw == "qwer" ) {
+                Toast.makeText(this, "관리자님 환영합니다", Toast.LENGTH_SHORT).show()
+            }
+            
+            
+            else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+
+
 
         }
 
